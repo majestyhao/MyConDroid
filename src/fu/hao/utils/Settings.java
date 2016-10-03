4d7be516b7e5068b050ee02f56b9ac1d7957db90
Hao Fu <jamesfuhao@gmail.com>
2016-10-03 12:44:37 -0700

    InstrumentationHelper

---
+++ src/fu/hao/utils/Settings.java
@@ -0,0 +1,113 @@
+/*
+* Copyright 2016 Hao Fu and contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* Contributors:
+*   Hao Fu
+*/
+
+package fu.hao.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Class: Settings
+ * Description:
+ * Authors: Hao Fu(haofu@ucdavis.edu)
+ * Date: 7/14/2016 6:00 PM
+ */
+public class Settings {
+	private static int logLevel = Log.MODE_MSG;
+	private static Set<String> targetMethods = new HashSet<String>();
+
+	private static String appDirectory = null;
+	private static String appName = null;
+	private static String outputDirectory = null;
+	private static String androidLib = null;
+
+	private static boolean printOutput = true;
+	private static boolean printConstraints = false;
+	private static boolean generateStats = false;
+
+	public static Set<String> getTargetMethods() {
+		return targetMethods;
+	}
+
+	public static void setTargetMethods(Set<String> targetMethods) {
+		Settings.targetMethods = targetMethods;
+	}
+
+	public static String getAppDirectory() {
+		return appDirectory;
+	}
+
+	public static void setAppDirectory(String appDirectory) {
+		Settings.appDirectory = appDirectory;
+	}
+
+	public static String getAppName() {
+		return appName;
+	}
+
+	public static void setAppName(String appName) {
+		Settings.appName = appName;
+	}
+
+	public static String getOutputDirectory() {
+		return outputDirectory;
+	}
+
+	public static void setOutputDirectory(String outputDirectory) {
+		Settings.outputDirectory = outputDirectory;
+	}
+
+	public static int getLogLevel() {
+		return logLevel;
+	}
+
+	public static void setLogLevel(int logLevel) {
+		Settings.logLevel = logLevel;
+	}
+
+	public static boolean getGenerateStats() {
+		return generateStats;
+	}
+
+	public static void setAndroidLib(String androidLib) {
+		Settings.androidLib = androidLib;
+	}
+
+	public static String getAndroidLib() {
+		return androidLib;
+	}
+
+	private enum TargetType {
+		METHODS,
+		NATIVE,
+		REFLECTION
+	}
+
+	static public TargetType Target = TargetType.METHODS;
+	static public Set<String> TargetMethods = new HashSet<String>();
+
+	static public String AppDirectory = null;
+	static public String AppName = null;
+	static public String OutputDirectory = null;
+
+	static public boolean PrintOutput = true;
+	static public boolean PrintConstraints = false;
+	static public boolean GenerateStats = false;
+
+}
---
+++ src/fu/hao/utils/Statistics.java
@@ -0,0 +1,65 @@
+package fu.hao.utils;
+
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Class: Statistics
+ * Description:
+ * Authors: Hao Fu(haofu@ucdavis.edu)
+ * Date: 7/14/2016 6:00 PM
+ */
+public class Statistics {
+    private static Date startTime = null;
+    private static Date callGraphStartTime = null;
+    private static Date callGraphEndTime = null;
+    private static Date constraintStartTime = null;
+    private static Date constraintEndTime = null;
+
+    private static long numberOfNodes = 0;
+    private static long numberOfEdges = 0;
+
+    public static void startAnalysis() {
+        if (Settings.getGenerateStats()) {
+            startTime = new Date();
+        }
+    }
+
+    public static void startCallGraph() {
+        if (Settings.getGenerateStats()) {
+            callGraphStartTime = new Date();
+        }
+    }
+
+    public static void endCallGraph() {
+        if (Settings.getGenerateStats()) {
+            callGraphEndTime = new Date();
+        }
+    }
+
+    public static void setNumberOfNodes(long numberOfNodes) {
+        Statistics.numberOfNodes = numberOfNodes;
+    }
+
+    public static void setNumberOfEdges(long numberOfEdges) {
+        Statistics.numberOfEdges = numberOfEdges;
+    }
+
+    public static void startConstraintAnalysis() {
+        if (Settings.getGenerateStats()) {
+            constraintStartTime = new Date();
+        }
+    }
+
+    public static void endConstraintAnalysis() {
+        if (Settings.getGenerateStats()) {
+            constraintEndTime = new Date();
+        }
+    }
+
+
+
+}
